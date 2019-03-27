0f610a1f2aa8b058b431846255328f3d00862950
daianegleyce <36937365+daianegleyce@users.noreply.github.com>
2019-03-27 13:58:31 -0300

    Add files via upload

---
+++ usjt_ccp3anmca_jpa_hibernate/src/main/java/br/usjt/persistence/TesteInsereUnUsuario.java
@@ -0,0 +1,23 @@
+package br.usjt.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import br.usjt.usjt.model.Usuario;
+import br.usjt.util.JPAUtil;
+
+public class TesteInsereUnUsuario {
+
+	public static void main(String[] args) {
+		EntityManager manager = JPAUtil.getEntityManager();
+		EntityTransaction transaction = manager.getTransaction();
+		transaction.begin();
+		Usuario u = new Usuario ();
+		u.setNome("Ana");
+		u.setFone("11223344");
+		u.setEmail("ana@usjt.br");
+		manager.persist(u);
+		transaction.commit();
+		manager.close();
+		JPAUtil.close();
+		}
+}
